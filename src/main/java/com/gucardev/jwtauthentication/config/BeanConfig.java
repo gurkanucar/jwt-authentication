ddb44608177760400f38ff4eafbc9d0265b753e6
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-18 13:32:19 +0300

    beans configured

---
+++ src/main/java/com/gucardev/jwtauthentication/config/BeanConfig.java
@@ -0,0 +1,16 @@
+package com.gucardev.jwtauthentication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class BeanConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+}
