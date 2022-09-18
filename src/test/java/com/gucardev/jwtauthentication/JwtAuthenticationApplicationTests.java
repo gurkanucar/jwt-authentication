d166cff97883ef69634cc03078de1c35c6efbfcc
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-18 13:27:35 +0300

    Initial commit

---
+++ src/test/java/com/gucardev/jwtauthentication/JwtAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gucardev.jwtauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAuthenticationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
