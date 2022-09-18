3d06e4a55f7e018a7667da00a45e23512ccffea4
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-18 13:30:56 +0300

    User service created

---
+++ src/main/java/com/gucardev/jwtauthentication/service/UserService.java
@@ -0,0 +1,32 @@
+package com.gucardev.jwtauthentication.service;
+
+
+import com.gucardev.jwtauthentication.exception.GeneralException;
+import com.gucardev.jwtauthentication.model.User;
+import com.gucardev.jwtauthentication.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class UserService {
+
+    private final UserRepository userRepository;
+
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public User create(User user) {
+        return userRepository.save(user);
+    }
+
+    public User findUserByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new GeneralException("user not found!", HttpStatus.NOT_FOUND));
+    }
+
+}
