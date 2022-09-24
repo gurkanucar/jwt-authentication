e61d3830bf4c33503fdd0f2d21ff0b0666e321c9
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-24 07:49:27 -0700

    UserDto added to TokenResponseDTO

--- src/main/java/com/gucardev/jwtauthentication/dto/TokenResponseDTO.java
+++ src/main/java/com/gucardev/jwtauthentication/dto/TokenResponseDTO.java
@@ -1,8 +1,15 @@
 package com.gucardev.jwtauthentication.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Data
 public class TokenResponseDTO {
     private String accessToken;
+    private UserDto user;
 }
