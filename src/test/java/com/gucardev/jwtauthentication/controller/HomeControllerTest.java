1159372b49744efc808ac949526ceab0bacae5fb
Gurkan UCAR <ucar.gurkan@hotmail.com>
2022-09-28 01:24:17 +0300

    admin controller integration test wrote

---
+++ src/test/java/com/gucardev/jwtauthentication/controller/HomeControllerTest.java
@@ -0,0 +1,52 @@
+package com.gucardev.jwtauthentication.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gucardev.jwtauthentication.dto.TokenResponseDTO;
+import com.gucardev.jwtauthentication.request.LoginRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext
+@AutoConfigureMockMvc
+class HomeControllerTest {
+
+
+    public final ObjectMapper mapper = new ObjectMapper();
+
+    @Autowired
+    public MockMvc mvc;
+
+    @Test
+    void admin() throws Exception {
+        var loginRequestAsString =
+                mapper.writeValueAsString(new LoginRequest("gurkan", "pass"));
+
+        MvcResult result = mvc.perform(post("/api/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(loginRequestAsString))
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(status().isOk())
+                .andReturn();
+        String content = result.getResponse().getContentAsString();
+        var loggedUser = mapper.readValue(content, TokenResponseDTO.class);
+        mvc.perform(get("/api/admin")
+                .accept(MediaType.APPLICATION_JSON)
+                .header(HttpHeaders.AUTHORIZATION, "Bearer " + loggedUser.getAccessToken())
+                .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().is2xxSuccessful());
+    }
+}
